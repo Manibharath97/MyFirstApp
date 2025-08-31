3753276d898198eb78c702bb398563ad5c7cb3c4
Manibharath97 <manibharath.ch@gmail.com>
2025-08-31 22:22:10 +0530

    Overview of Constructor Dependency Injection

--- src/main/java/com/codesnippet/MyFirstApp/Components/OnlineOrder.java
+++ src/main/java/com/codesnippet/MyFirstApp/Components/OnlineOrder.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OnlineOrder{
 
-	/*public OnlineOrder() {
+	public OnlineOrder() {
 		System.out.println("Init OnlineOrder");
-	}*/
+	}
 }
