cabed2962d9cfc4d5eb300f2aae03f031ad74978
Manibharath97 <manibharath.ch@gmail.com>
2025-08-31 21:53:57 +0530

    Overview of Field Dependency Injection

--- src/main/java/com/codesnippet/MyFirstApp/Components/OnlineOrder.java
+++ src/main/java/com/codesnippet/MyFirstApp/Components/OnlineOrder.java
@@ -3,9 +3,9 @@ package com.codesnippet.MyFirstApp.Components;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OnlineOrder implements Order{
+public class OnlineOrder{
 
-	public OnlineOrder() {
+	/*public OnlineOrder() {
 		System.out.println("Init OnlineOrder");
-	}
+	}*/
 }
